82ae2c744864f1a65769fc48da080a00b17b8b1b
cameron taggart <cameron.taggart@fluke.com>
2012-03-19 17:55:30 -0700

    WebSharper HelloWorld

---
+++ WebSharper/HelloWorld/HelloWorld.fs
@@ -0,0 +1,22 @@
+﻿namespace Samples
+
+open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.Html
+
+module HelloWorld =
+
+    [<JavaScript>]
+    let Main () =
+        let welcome = P [Text "Welcome"]
+        Div [
+            welcome
+            Button [Text "Click Me!"]
+            |>! OnClick (fun e args ->
+                welcome.Text <- "Hello, world!")
+        ]
+
+type HelloWorldViewer() =
+    inherit Web.Control()
+
+    [<JavaScript>]
+    override this.Body = HelloWorld.Main () :> Html.IPagelet
---
+++ WebSharper/HelloWorld/Site.fs
@@ -0,0 +1,37 @@
+﻿namespace Samples
+
+open IntelliFactory.WebSharper.Sitelets
+
+module SampleSite =
+    open IntelliFactory.Html
+
+    type Action = 
+        | HelloWorld
+
+    let helloWorld =
+        PageContent <| fun context ->
+            {
+                Page.Default with
+                    Doctype = Some "<!DOCTYPE html>" // HTML5
+                    Title = Some "Sample - HelloWorld"
+                    Body = [ Div [ new HelloWorldViewer() ] ]
+            }  
+
+open SampleSite
+
+type SampleWebsite() =
+    interface IWebsite<Action> with
+        
+        member this.Actions =
+            [
+                HelloWorld
+            ]
+        
+        member this.Sitelet =
+            [
+                Sitelet.Content "/HelloWorld" HelloWorld helloWorld
+            ]
+            |> Sitelet.Sum
+
+[<assembly: WebsiteAttribute(typeof<SampleWebsite>)>]
+do ()
